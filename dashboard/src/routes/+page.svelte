9e340d987241296371745e0e92a747df13ad4212
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-05-18 19:23:01 +0800

    fix: update link in the page component to point to the correct URL

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -226,7 +226,7 @@
     class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
   >
     <div class="flex w-full max-w-sm flex-col gap-6">
-      <a href="##" class="flex items-center gap-2 self-center font-medium">
+      <a href="https://oddinpay.com" class="flex items-center gap-2 self-center font-medium">
         <div
           class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
         >
