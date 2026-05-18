54f09949520696315a47b9e6bdefa1e9279452bd
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-05-18 19:10:08 +0800

    refactor: clean up svelte.config.js by removing commented code and ensuring consistent import formatting

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -1,20 +1,12 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { preprocessMeltUI, sequence } from '@melt-ui/pp'
-
-// import path from 'node:path';
-// import { fileURLToPath } from 'node:url';
-
-// const dirname = path.resolve(fileURLToPath(import.meta.url), '../');
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 
 const adapterOptions = { precompress: true };
 
 const config = {
   extensions: [".svelte", ".svx", ".md"],
-  preprocess: sequence([
-    vitePreprocess(),
-    preprocessMeltUI() 
-  ]),
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
   kit: {
     adapter: adapter(adapterOptions),
     alias: {
