7ef0608de7b6fd0a61ae10d7968bd3efa6126eb9
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-05-19 15:54:03 +0800

    fix: remove commented-out Connect menu item from user menu component

--- dashboard/src/lib/components/_extras/navbars/user-menu.svelte
+++ dashboard/src/lib/components/_extras/navbars/user-menu.svelte
@@ -60,16 +60,6 @@
         </span>
       </DropdownMenuLabel>
       <DropdownMenuSeparator class="bg-zinc-600" />
-      <!-- <DropdownMenuGroup>
-			<DropdownMenuSeparator class="bg-zinc-600" />
-			<DropdownMenuItem
-				onclick={() => goto('/connect')}
-				class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white"
-			>
-				<Unplug size={16} class="opacity-80" aria-hidden="true" />
-				<span>Connect</span>
-			</DropdownMenuItem>
-		</DropdownMenuGroup> -->
       <DropdownMenuGroup>
         <DropdownMenuSeparator class="bg-zinc-600" />
         <DropdownMenuItem
