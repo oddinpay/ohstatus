ba872f696712d95d66ce4ab409d75da7ef428a47
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-05-31 19:55:41 +0800

    fix: update button hover effect in Dialog footer for improved visibility

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -106,7 +106,7 @@
         <Dialog.Footer>
           <Form.Button
             formaction="?/create"
-            class="w-full flex items-center justify-center bg-black text-white transition-[background-color,transform,opacity] duration-200 ease-out hover:bg-zinc-800 hover:text-white active:scale-[0.98] disabled:pointer-events-auto disabled:cursor-not-allowed cursor-pointer"
+            class="w-full flex items-center justify-center bg-black text-white transition-[background-color,transform,opacity] duration-200 ease-out hover:bg-zinc-700 hover:text-white active:scale-[0.98] disabled:pointer-events-auto disabled:cursor-not-allowed cursor-pointer"
             type="submit"
             variant="outline"
             disabled={$submitting}
