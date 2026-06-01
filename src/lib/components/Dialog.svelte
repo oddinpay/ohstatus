49f0e5b9a6ccae2306c56c083825560a69ca65b3
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-06-01 23:02:58 +0800

    fix: reduce checkmark SVG size for improved layout

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -191,8 +191,8 @@
       stroke-width: 3;
       stroke: #086623;
       stroke-miterlimit: 10;
-      width: 120px;
-      height: 120px;
+      width: 80px;
+      height: 80px;
     }
 
     .checkmark__circle2 {
