e7fc92bb643d7840796b035432045b3b59dc740e
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-06-28 02:36:13 +0800

    fix: simplify conditional statement formatting in hooks.server.js

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,15 +1,8 @@
 import { dev } from "$app/environment";
 
 export function handle({ event, resolve }) {
-  if (
-    dev &&
-    event.url.pathname === "/.well-known/appspecific/com.chrome.devtools.json"
-  ) {
+  if (dev && event.url.pathname === "/.well-known/appspecific/com.chrome.devtools.json") {
     return new Response(undefined, { status: 404 });
-  }
-
-  if (typeof import.meta.url === "undefined") {
-    globalThis.import = { meta: { url: "file:///app/" } };
   }
 
   return resolve(event);
