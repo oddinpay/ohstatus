76d8ccec7d79a6a11298e754ca40850b2db611f5
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-07-10 21:29:07 +0800

    fix: update tooltip background color for improved visibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1112,7 +1112,7 @@
                                     </Tooltip.Trigger>
 
                                     <Tooltip.Content
-                                      class="bg-[#1e293b] rounded-md text-white shadow-md shadow-black/20"
+                                      class="bg-black rounded-md text-white shadow-md shadow-black/20"
                                     >
                                       <div
                                         class="flex flex-col items-center justify-center p-2 text-sm"
