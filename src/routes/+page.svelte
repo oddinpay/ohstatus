e9318c8855172b1f88eed964fbda42f4130aed93
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-07-10 23:39:05 +0800

    fix: update Tooltip.Content class for improved styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1112,7 +1112,7 @@
                                     </Tooltip.Trigger>
 
                                     <Tooltip.Content
-                                      class="bg-black rounded-md text-white shadow-md shadow-black/20"
+                                      class="bg-black rounded-md text-white border-solid border-2 border-gray-400"
                                     >
                                       <div
                                         class="flex flex-col items-center justify-center p-2 text-sm"
