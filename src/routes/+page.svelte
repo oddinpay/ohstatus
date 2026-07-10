eb9c9ce73a90b145b9340472ed18584e598355cf
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-07-10 21:43:24 +0800

    fix: update chip border class for improved styling and adjust tooltip content class for consistency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1104,7 +1104,7 @@
                                       {#snippet child({ props })}
                                         <div
                                           {...props}
-                                          class="chip border-dark-10 border {s.status} {i ===
+                                          class="chip border-border-input border {s.status} {i ===
                                           dayIndex
                                             ? s.status
                                             : ''}"
@@ -1113,7 +1113,7 @@
                                     </Tooltip.Trigger>
 
                                     <Tooltip.Content
-                                      class="bg-black rounded-md text-white shadow-md shadow-black/20"
+                                      class="bg-black rounded-md  text-white shadow-md shadow-black/20"
                                     >
                                       <div
                                         class="flex flex-col items-center justify-center p-2 text-sm"
