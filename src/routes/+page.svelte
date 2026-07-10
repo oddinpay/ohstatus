64d416257c8b6b0b386b30b82d05adbf367a199a
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-07-10 23:42:31 +0800

    fix: remove border class from Tooltip.Content for improved styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1112,7 +1112,7 @@
                                     </Tooltip.Trigger>
 
                                     <Tooltip.Content
-                                      class="bg-black rounded-md text-white shadow-md shadow-black/20 border-solid border-2 border-zinc-700"
+                                      class="bg-black rounded-md text-white shadow-md shadow-black/20"
                                     >
                                       <div
                                         class="flex flex-col items-center justify-center p-2 text-sm"
