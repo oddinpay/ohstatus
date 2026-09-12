083e234e82e589dd732d79a6e2faad1772845afc
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-09-13 06:10:31 +0800

    Fix: update import statement for sitemap response handling

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,11 +1,11 @@
 // /src/routes/sitemap.xml/+server.ts
-import * as sitemap from "super-sitemap/sveltekit";
+import { response } from "super-sitemap/sveltekit";
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ url }) => {
   const fullHostname = `${url.protocol}//${url.host}`;
 
-  return await sitemap.response({
+  return await response({
     origin: fullHostname,
     excludeRoutePatterns: [/^\/unsubscribe.*/],
   });
